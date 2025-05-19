0eea9d1caebbcff577b2c039df1dba367a70dbc5
brayanprzy041 <brayan.przysbeczyski@escola.pr.gov.br>
2025-05-19 08:32:31 -0300

    Add files via upload

--- ex3/script.js
+++ ex3/script.js
@@ -8,33 +8,77 @@ function sobra() {
     num2 = Number(window.prompt('Digite outro numero:'));
 
     resultado = num1 % num2;
+    numero = resultado % 2;
     alert("O resultado é " + resultado + ".");
+
+    if( numero== 0) {
+        alert ("Par");
+    }else{
+        alert ("Impar")
+
+}
+
 }
 function multiplicacao() {
     num1 = Number(window.prompt('Digite um numero:'));
     num2 = Number(window.prompt('Digite outro numero:'));
 
     resultado = num1 * num2;
+    numero = resultado % 2;
     alert("O resultado é " + resultado + ".");
+
+    if( numero== 0) {
+        alert ("Par");
+    }else{
+        alert ("Impar")
+
+}
 }
 function adicao() {
     num1 = Number(window.prompt('Digite um numero:'));
     num2 = Number(window.prompt('Digite outro numero:'));
 
     resultado = num1 + num2;
+    numero = resultado % 2;
     alert("O resultado é " + resultado + ".");
+
+    if( numero== 0) {
+        alert ("Par");
+    }else{
+        alert ("Impar")
+
+}
 }
 function subitracao() {
     num1 = Number(window.prompt('Digite um numero:'));
     num2 = Number(window.prompt('Digite outro numero:'));
 
     resultado = num1 - num2;
+    numero = resultado % 2;
+
     alert("O resultado é " + resultado + ".");
+    if( numero== 0) {
+        alert ("Par");
+    }else{
+        alert ("Impar")
+
 }
+}
+
 function divisao() {
     num1 = Number(window.prompt('Digite um numero:'));
     num2 = Number(window.prompt('Digite outro numero:'));
 
     resultado = num1 / num2;
+    numero = resultado % 2;
+
     alert("O resultado é " + resultado + ".");
+
+    if( numero== 0) {
+        alert ("Par");
+    }else{
+        alert ("Impar")
+
+}
+
 }
