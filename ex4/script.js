0d83f42fbbb18b51eca2330907a487e5bd190168
brayanprzy041 <brayan.przysbeczyski@escola.pr.gov.br>
2025-05-26 11:27:04 -0300

    Add files via upload

---
+++ ex4/script.js
@@ -0,0 +1,29 @@
+function media () {
+    let nota1 = Number(window.prompt("Digite a primeira nota: "));
+    let nota2 = Number(window.prompt("Digite a segunda nota: "));
+    let nota3 = Number(window.prompt("Digite a terceira nota: "));
+
+
+    let media = (nota1 + nota2 + nota3) / 3;
+
+    let resultado = document.querySelector('#resultado');
+    resultado.innerHTML = `<p>O resultado é ${media}</p>`
+    
+    console.log (resultado)
+
+
+  if (media > 59) {
+     let resultado = document.querySelector('#resultado');
+    resultado.innerHTML = `<h2><p> Você já está na media </p>`;
+    
+
+    } else {
+        let sobra = 59 - media
+
+         let resultado = document.querySelector('#resultado');
+    resultado.innerHTML = `<h2><p>Você ainda não está na media, ainda faltam${sobra} pontos</p></h2>`;
+    
+      
+    }
+
+}
