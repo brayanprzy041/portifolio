7f8868becec50be460aebce9271d38f02f2d9d4b
brayanprzy041 <brayan.przysbeczyski@escola.pr.gov.br>
2025-06-16 11:42:07 -0300

    Add files via upload

--- ex7/script7.js
+++ ex7/script7.js
@@ -1,20 +1,19 @@
 let mostraResultado = document.querySelector("#resultado");
 let inscritos = 0;
-let times = 0;
 
 function cliques(){
 
     inscritos += 1;
-    times = parseInt( inscritos);
+    times = parseInt(inscritos);
 
-    mostraResultado.innerHTML =`<p> Número de cliques: ${inscritos}</p><p>Números de times: ${times}.</p>`;
+    mostraResultado.innerHTML =`<p> Número de cliques: ${inscritos}`;
 
-    if(times >=10){
-        
-        ("é possível iniciar o campeonato, pois temos 4 equipes!")
-    }r
+    if(inscritos >=10){
+            
+    }
 }
 
-function zerar(){
-
-}
+    function zerar(){
+        inscritos = 0;
+        mostraResultado.innerHTML = `<p>numero de cliques 0</p>`;
+    }
